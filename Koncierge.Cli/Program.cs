27b3ef0020a8f269af8c45e2e5b10ceb872e4069
Davide <davide.maggi@live.com>
2023-12-27 22:23:00 +0100

    Added Cli

---
+++ Koncierge.Cli/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Koncierge.Cli
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
