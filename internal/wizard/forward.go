cb6faebd3bb1c3569e7c050ac58f714a6ed67a52
Davide Maggi <davide.maggi@accenture.com>
2025-05-15 10:14:16 +0200

    Ports retrieved

--- internal/wizard/forward.go
+++ internal/wizard/forward.go
@@ -1,6 +1,7 @@
 package wizard
 
 import (
+	"fmt"
 	"github.com/davidemaggi/koncierge/internal"
 	"github.com/davidemaggi/koncierge/internal/config"
 	"github.com/davidemaggi/koncierge/internal/k8s"
@@ -24,12 +25,37 @@ func BuildForward() internal.ForwardDto {
 	fwdtypes = append(fwdtypes, internal.ForwardPod)
 
 	ret.ForwardType, _ = pterm.DefaultInteractiveSelect.WithOptions(fwdtypes).Show()
-
+	var ports []internal.ServicePortDto
 	if ret.ForwardType == internal.ForwardService {
 		ret.TargetName, _ = pterm.DefaultInteractiveSelect.WithOptions(k8s.GetServicesInNamespace(ret.Namespace)).Show()
-		//ports := k8s.GetServicePorts(ret.Namespace, ret.TargetName) // Arrivato qui
+		ports = k8s.GetServicePorts(ret.Namespace, ret.TargetName) // Arrivato qui
 
 	}
+
+	if ret.ForwardType == internal.ForwardPod {
+		// TODO: get Pod ports
+
+	}
+
+	var portOptions []string
+
+	portMap := make(map[string]internal.ServicePortDto)
+
+	for _, port := range ports {
+		tmpKey := fmt.Sprintf("%d (%s)", port.PodPort, port.Protocol)
+		portOptions = append(portOptions, tmpKey)
+		portMap[tmpKey] = port
+	}
+
+	selectedName, _ := pterm.DefaultInteractiveSelect.
+		WithOptions(portOptions).
+		WithDefaultText("Select a port").
+		Show()
+
+	// Retrieve full object based on name
+	selectedPort := portMap[selectedName]
+
+	_ = selectedPort
 
 	return ret
 }
