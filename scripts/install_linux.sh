a44ae420d50de7ec5e5d8b88e04697baf2532703
Davide Maggi <davide.maggi@accenture.com>
2025-07-25 11:30:38 +0200

    Fixed List UI Added Linux Install script

---
+++ scripts/install_linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# allow specifying different destination directory
+DIR="${DIR:-"$HOME/.local/bin"}"
+
+ARCH=$(uname -m)
+
+# prepare the download URL
+GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/davidemaggi/koncierge/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+GITHUB_FILE="Koncierge_${GITHUB_LATEST_VERSION//v/}_Linux_${ARCH}.tar.gz"
+
+GITHUB_URL="https://github.com/davidemaggi/Koncierge/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
+
+
+# install/update the local binary
+curl -L -o koncierge.tar.gz $GITHUB_URL
+tar xzvf koncierge.tar.gz koncierge
+install -Dm 755 koncierge -t "$DIR"
+rm Koncierge koncierge.tar.gz
